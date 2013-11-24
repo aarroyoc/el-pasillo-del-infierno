a969d6dc7444f0a73fcd427ef440d857cac2ba5b
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2013-11-24 14:24:48 +0100

    RequireJS, first translations, first index

---
+++ js/translations.js
@@ -0,0 +1,10 @@
+/* translations.js */
+
+var translations = {
+  en:  { hello: "Hello, World!"
+       , bye:   "Goodbye!"
+       }
+, es:  { hello: "¡Hola, Mundo!"
+       , bye:   "¡Adiós!"
+       }
+};
